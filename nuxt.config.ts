1e7e1c012c0dffeb508f4cfea7a39871b0e204d5
Blake Kostner <git@btkostner.io>
2021-12-10 22:49:41 -0700

    setup windi css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 import { defineNuxtConfig } from 'nuxt3'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  buildModules: [
+    'nuxt-windicss'
+  ],
 
+  windicss: {
+    analyze: true
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="bg-red-200">
     <h1>blablabl HI!</h1>
   </div>
 </template>
